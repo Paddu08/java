5c1b94c7739d2228d0db08e34b641be2e02d2f7a
Paddu08 <padmanabhan123345@gmail.com>
2022-12-16 10:18:57 +0530

    git commit -m"hello"

---
+++ frame_test.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;;
+public class frame_test
+ {
+    public static void main(String args[]) {
+        JFrame frame=new JFrame();;
+        frame.setSize(500, 500);
+        frame.setVisible(true);
+        
+        
+    }
+    
+}
