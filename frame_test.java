fdc339607c396bf4a815a30a5645238cd7fe723e
Paddu08 <padmanabhan123345@gmail.com>
2022-12-16 20:35:00 +0530

    label

--- frame_test.java
+++ frame_test.java
@@ -1,10 +1,18 @@
-import javax.swing.JFrame;;
+import javax.swing.Icon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;;
 public class frame_test
  {
     public static void main(String args[]) {
         JFrame frame=new JFrame();;
+        JLabel label=new JLabel();
+        label.setText("Hello");
+
+    
         frame.setSize(500, 500);
         frame.setVisible(true);
+        frame.add(label);
+        
         
         
     }
